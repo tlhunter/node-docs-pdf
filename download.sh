2360d549761e37c7a36b7f1dec768aebdc81b597
Thomas Hunter II <me@thomashunter.name>
2019-12-19 23:53:01 -0800

    dropping v6 and v11, adding v12 and v13

--- download.sh
+++ download.sh
@@ -1,11 +1,11 @@
-echo "v11"
-wget https://nodejs.org/docs/latest/api/all.html -O v11.html
+echo "v13"
+wget https://nodejs.org/docs/latest/api/all.html -O v13.html
+
+echo "v12"
+wget https://nodejs.org/docs/latest-v12.x/api/all.html -O v12.html
 
 echo "v10"
 wget https://nodejs.org/docs/latest-v10.x/api/all.html -O v10.html
 
 echo "v8"
 wget https://nodejs.org/docs/latest-v8.x/api/all.html -O v8.html
-
-echo "v6"
-wget https://nodejs.org/docs/latest-v6.x/api/all.html -O v6.html
